7b8eb8a66d31033d051ce794ccfb35ffba50f14a
15edha <15edha@stockholmscience.se>
2019-09-16 21:54:55 +0200

    ok

--- lab7testing1.py
+++ lab7testing1.py
@@ -72,6 +72,70 @@ main()
 
 x = q.search("Bulbasaur")
 
+KAPACITET = 500
+
+class Node: #Själva verket en node datastruktor, som en linkedlist node
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.nesta = None
+
+class Hashtable:
+    #initisiserar en hash tabell
+    def __init__(self, size):
+        self.size = size
+        self.kapasitet = KAPACITET
+        self.luckor = [None] * self.kapasitet
+
+    def store(self, key, data):
+        #steg 1 blir att öka storleken
+        self.size += 1
+        #steg 2 blir att beräkna index av key
+        index = self.hashfunction(key)
+
+        node = self.luckor[index] #går till noden
+
+        #om luckan är tom så skapas ny node och retunerar
+        if node is None:
+            self.luckor[index] = Node(key, data)
+            return
+
+
+    #annars gå igenomen länkade listan på given index, sedan lägg till ny node på slutet av listan
+        tidigare = node
+        while node is not None:
+            tidigare = node
+            node = node.nesta
+
+        tidigare.nesta = Node(key, data)
+
+    def search(self, key):
+        #steg 1 blir att beräkna hashen
+        index = self.hashfunction(key)
+
+        node = self.luckor[index] #går till första noden i listan
+
+        while node is not None and node.key != key: #korsar den länkade listan på noden
+            node = node.nesta
+
+        if node is None:
+            raise KeyError
+        else:
+            return node.data
+
+    def hashfunction(self, key):
+        hashsumman = 0
+        for i, c in enumerate(key): #för varje karaktär i key
+            hashsumman += (i+len(key)) ** ord(c) #lägger till index ? längden av key upphöjt till nuvarnde char Node
+            hashsumman = hashsumman % self.size
+
+            return hashsumman
+
+
+
+
+
+
 
 
 
