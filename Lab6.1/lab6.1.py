c66eafa6174c3cf869a04f557eb948008f8040d6
Edris <edris12@hotmail.com>
2019-09-30 15:14:37 +0200

    labb3

---
+++ Lab6.1/lab6.1.py
@@ -0,0 +1,62 @@
+import timeit
+
+class Enfinlat:
+    def __init__(self, trackensid, lotid, artistensnamn, latenstitel):
+        self.trackensid = trackensid
+        self.lotid = lotid
+        self.artistensnamn = artistensnamn
+        self.latenstitel = latenstitel
+
+    def __str__(self):
+        return "TrackID: " + self.trackensid + " låtid: " + self.lotid + " artistnamn: " + self.artistensnamn + \
+               " åttitel: " + self.latenstitel
+
+    def __lt__(self, other):
+        return self.artistensnamn < other.artistensnamn
+
+    def tafram_artisten(self):
+        return self.artistensnamn
+
+
+def lasafilen(dencoolafilen):
+    okjektlista = []
+    objektdictianry = {}
+
+    with open(dencoolafilen, encoding="utf8") as track_file:
+        allarader = track_file.readlines()
+
+        for rader in allarader:
+            saken = rader.strip().split("<SEP>")
+            okjektlista.append(Enfinlat(saken[0], saken[1], saken[2], saken[3]))
+            objektdictianry[saken[2]] = okjektlista[len(okjektlista)-1]
+
+    return okjektlista, objektdictianry
+
+
+def linsok(listan, namnetpaartisten):
+    hittad = False
+
+    for saken in listan:
+        if saken.artistensnamn == namnetpaartisten:
+            hittad = True
+
+    return hittad
+
+def main():
+
+    filename = "unique_tracks.txt"
+
+    lista, dicten = lasafilen(filename)
+    n = len(lista)
+    print("Antal element =", n)
+
+    sista = lista[n-1]
+    testartist = sista.artistensnamn
+
+    linjtid = timeit.timeit(stmt = lambda: linsok(lista, testartist), number = 10)
+    print("Linjärsökningen tog", round(linjtid, 4) , "sekunder")
+
+
+
+if __name__ == '__main__':
+    main()
