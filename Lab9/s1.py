5c890a20f62d1f2719a9da0fa62cd41f8fc183a8
15edha <15edha@stockholmscience.se>
2019-11-05 10:58:46 +0100

    ok

--- Lab9/s1.py
+++ Lab9/s1.py
@@ -1,10 +1,12 @@
 import sys
 import string
 
+
 class Node:
-   def __init__(self, x, next = None):
-      self.value = x
-      self.next = next
+    def __init__(self, x, next=None):
+        self.value = x
+        self.next = next
+
 
 class LinkedQ():
 
@@ -18,7 +20,6 @@ class LinkedQ():
             return True
         else:
             return False
-
 
     def enqueue(self, x):
         new = Node(x)
@@ -43,12 +44,12 @@ class LinkedQ():
         self._first = None
         self._last = None
 
-
     def peek(self):
         if not self.isEmpty():
             return self._first.value
         else:
             return None
+
 
 """
 <formel>::= <mol> \n
@@ -61,71 +62,79 @@ class LinkedQ():
 """
 q = LinkedQ()
 
-
 pairing = []
 
-ALLAATOMER = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Fl', 'Lv']
+ALLAATOMER = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K',
+              'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb',
+              'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs',
+              'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf',
+              'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
+              'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs',
+              'Mt', 'Ds', 'Rg', 'Cn', 'Fl', 'Lv']
 
 
 class Syntaxfel(Exception):
     pass
 
-def storemol(themol): #Lagrar molekyl i en kö
-    for everychar in themol: #varje karaktär i molykylen läggs in i kön
+
+def storemol(themol):  # Lagrar molekyl i en kö
+    for everychar in themol:  # varje karaktär i molykylen läggs in i kön
         q.enqueue(everychar)
     return q
 
 
-
 def readmol():
-    readgroup() #Läser gruppen
+    readgroup()  # Läser gruppen
     if q.peek() == ")":
         if len(pairing) < 1:
             raise Syntaxfel("Felaktig gruppstart vid radslutet ")
         else:
             return
     elif q.peek():
-        if q.peek() == "(" or q.peek().isalpha():
+        if q.peek() == "(" or q.peek().isupper() or q.peek().islower():
             readmol()
     elif q.isEmpty():
         return
     else:
         readmol()
 
-def readgroup():
 
-    if q.isEmpty(): #Om kön är tom, raise syntaxfel
+def readgroup():
+    if q.isEmpty():  # Om kön är tom, raise syntaxfel
         raise Syntaxfel("Felaktig gruppstart vid radslutet ")
 
-    if q.peek().isalpha(): #kollar om det är bokstav
-        readatom() #Läser atomen
-        if q.peek() is None: #Om peeken är inget
+    if q.peek().isupper() or q.peek().islower():  # kollar om det är bokstav
+        readatom()  # Läser atomen
+        if q.peek() is None:  # Om peeken är inget
             return
 
-        if q.peek().isdigit(): #Om nästa tecken är nummer
-            readnum() #Anropa funktionen där numret läses
+        if q.peek().isdigit():  # Om nästa tecken är nummer
+            readnum()  # Anropa funktionen där numret läses
         return
 
-    elif q.peek() == "(": #Om en grupp blrjar med (, ok! Läggs in i kön
-        pairing.append(q.dequeue()) #Läggs in i kön gruppvis
-        readmol() #Anropar funktion där molykylen läses
-        if q.peek() != ")": #Eftersom en grupp måste ha () blir det fel om det inte slutar på)
+    if q.peek().isdigit():
+        raise Syntaxfel("Felaktig gruppstart vid radslutet ")
+
+
+    elif q.peek() == "(":  # Om en grupp börjar med (, ok! Läggs in i kön
+        pairing.append(q.dequeue())  # Läggs in i kön gruppvis
+        readmol()  # Anropar funktion där molykylen läses
+        if q.peek() != ")":  # Eftersom en grupp måste ha () blir det fel om det inte slutar på)
             raise Syntaxfel("Saknad högerparentes vid radslutet ")
-        if q.isEmpty() or q.peek().isdigit(): #Kan ej börja med tal, blir raise. Och kan ej vara tom
+        if q.isEmpty() or q.peek().isdigit():  # Kan ej börja med tal, blir raise. Och kan ej vara tom
             raise Syntaxfel("Saknad siffra vid radslutet ")
 
         else:
-            pairing.pop() #Ifall inget funkar tas pairingen bort
-            q.dequeue() #Bort från kön
-            if q.isEmpty(): #Eller om kön är tom blir det ju fel.
+            pairing.pop()  # Ifall inget funkar tas pairingen bort
+            q.dequeue()  # Bort från kön
+            if q.isEmpty():  # Eller om kön är tom blir det ju fel.
                 raise Syntaxfel("Saknad siffra vid radslutet ")
-        readnum() #Numret läses då när grupp inte finns
-    else: #Annars blir det fel gruppstart
+        readnum()  # Numret läses då när grupp inte finns
+    else:  # Annars blir det fel gruppstart
         raise Syntaxfel("Felaktig gruppstart vid radslutet ")
 
 
 def readatom():
-
     if q.peek().isupper():
         first = q.dequeue()
 
@@ -136,7 +145,6 @@ def readatom():
         if q.peek().islower():
             first = first + q.dequeue()
 
-
     if first in ALLAATOMER:
         return
     else:
@@ -144,30 +152,33 @@ def readatom():
 
 
 def readnum():
+    if q.peek().isdigit():
+        if q.peek() == "0":
+            q.dequeue()
+            raise Syntaxfel("För litet tal vid radslutet ")
 
-	if q.peek().isdigit():
-		if q.peek() == "0":
-			q.dequeue()
-			raise Syntaxfel("För litet tal vid radslutet ")
+        nummer = ""
+        while not q.isEmpty():
+            if q.peek() == None:
+                pass
+            elif q.peek().isdigit():
+                nummer += q.dequeue()
+            else:
+                break
+        if int(nummer) < 2:
+            raise Syntaxfel("För litet tal vid radslutet ")
+        else:
+            return
+    else:
+        raise Syntaxfel("Saknad siffra vid radslutet ")
 
-		num = ""
-		while q.peek() != None:
-			if q.peek().isdigit():
-				num = num + q.dequeue()
-			else:
-				break
-		if int(num) >= 2:
-			return
-		else:
-			raise Syntaxfel("För litet tal vid radslutet ")
-	else:
-		raise Syntaxfel("Saknad siffra vid radslutet ")
 
 def printQ():
-	items = ""
-	while not q.isEmpty():
-		items = items + q.dequeue()
-	return items
+    items = ""
+    while not q.isEmpty():
+        items = items + q.dequeue()
+    return items
+
 
 def readformel(themol):
     q = storemol(themol)
@@ -180,15 +191,17 @@ def readformel(themol):
     except Syntaxfel as error:
         return str(error) + printQ()
 
+
 def main():
-        themol = sys.stdin.readline().strip()
-        if themol != "#":
-            result = readformel(themol)
-            del pairing[:]
-            printQ()
-            q.empty()
-            print(result)
-            main()
+    themol = sys.stdin.readline().strip()
+    if themol != "#":
+        result = readformel(themol)
+        del pairing[:]
+        printQ()
+        q.empty()
+        print(result)
+        main()
+
 
 if __name__ == '__main__':
     main()
